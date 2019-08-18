c75e050fdeeca99157b4e0a2bd7a0f941b59a125
Thorsten Huhn <thorstenhuhn@me.com>
2019-08-18 13:46:59 +0200

    Fixed issue where wrong dashboard was used

--- AmazonWebServicesResource.popclipext/script.sh
+++ AmazonWebServicesResource.popclipext/script.sh
@@ -2,8 +2,6 @@ if [[ $POPCLIP_TEXT == ami-* ]]; then
 	open "https://$POPCLIP_OPTION_AWSREGION.console.aws.amazon.com/ec2/v2/home?region=$POPCLIP_OPTION_AWSREGION#Images:visibility=owned-by-me;search=$POPCLIP_TEXT;sort=desc:creationDate"
 elif [[ $POPCLIP_TEXT == eni-* ]]; then
 	open "https://$POPCLIP_OPTION_AWSREGION.console.aws.amazon.com/ec2/v2/home?region=$POPCLIP_OPTION_AWSREGION#NIC:search=$POPCLIP_TEXT;sort=desc:tag:Name"
-elif [[ $POPCLIP_TEXT == i-* ]]; then
-	open "https://$POPCLIP_OPTION_AWSREGION.console.aws.amazon.com/ec2/v2/home?region=$POPCLIP_OPTION_AWSREGION#Instances:search=$POPCLIP_TEXT;sort=desc:launchTime"
 elif [[ $POPCLIP_TEXT == sg-* ]]; then
 	open "https://$POPCLIP_OPTION_AWSREGION.console.aws.amazon.com/ec2/v2/home?region=$POPCLIP_OPTION_AWSREGION#SecurityGroups:search=$POPCLIP_TEXT;sort=groupName"
 elif [[ $POPCLIP_TEXT == snap-* ]]; then
@@ -14,6 +12,8 @@ elif [[ $POPCLIP_TEXT == vol-* ]]; then
 	open "https://$POPCLIP_OPTION_AWSREGION.console.aws.amazon.com/ec2/v2/home?region=$POPCLIP_OPTION_AWSREGION#Volumes:search=$POPCLIP_TEXT;sort=desc:createTime"
 elif [[ $POPCLIP_TEXT == vpc-* ]]; then
 	open "https://$POPCLIP_OPTION_AWSREGION.console.aws.amazon.com/vpc/home?region=$POPCLIP_OPTION_AWSREGION#vpcs:filter=$POPCLIP_TEXT"
+elif [[ $POPCLIP_TEXT == i-* ]]; then
+	open "https://$POPCLIP_OPTION_AWSREGION.console.aws.amazon.com/ec2/v2/home?region=$POPCLIP_OPTION_AWSREGION#Instances:search=$POPCLIP_TEXT;sort=desc:launchTime"
 else:
 	# ip address
 	open "https://$POPCLIP_OPTION_AWSREGION.console.aws.amazon.com/ec2/v2/home?region=$POPCLIP_OPTION_AWSREGION#Instances:search=$POPCLIP_TEXT;sort=desc:launchTime"
